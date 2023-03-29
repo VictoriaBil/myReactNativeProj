273d02c9b7be12a5b4042d9b29cca4cfad8cd137
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-03-29 17:34:52 +0300

    updates

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
+import { LoginScreen } from "./Screens/LoginScreen";
 
 export default function App() {
   return (
